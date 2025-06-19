31ca60fda97244aaaed89216767040503bf688da
kunstaytrue <st10487104@rcconnect.edu.za>
2025-06-19 09:42:41 +0200

    added a mutable list of , this stores all added packingt items

--- app/src/main/java/com/example/mymusic/MainActivity.kt
+++ app/src/main/java/com/example/mymusic/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -19,11 +20,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent { //Andile Kundayi ST10487104
             var currentScreen = remember { mutableStateOf("main") }
             var theSongName = remember { mutableStateOf("") }
             var theArtistsName = remember { mutableStateOf("") }
             var theRating = remember { mutableStateOf("") }
             var userComment = remember { mutableStateOf("") }
             var showError = remember { mutableStateOf(false) }
+            val  playListManager = remember { mutableStateListOf<playListManager>() }
         }
