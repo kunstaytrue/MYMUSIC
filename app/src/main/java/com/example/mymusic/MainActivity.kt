8ebfe5bdb273a422ae67f6298c700e8452664666
kunstaytrue <st10487104@rcconnect.edu.za>
2025-06-19 09:48:25 +0200

    i did this create a structure and column arrangements

--- app/src/main/java/com/example/mymusic/MainActivity.kt
+++ app/src/main/java/com/example/mymusic/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,9 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mymusic.ui.theme.MYMUSICTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,4 +32,12 @@ class MainActivity : ComponentActivity() {
             var userComment = remember { mutableStateOf("") }
             var showError = remember { mutableStateOf(false) }
             val  playListManager = remember { mutableStateListOf<playListManager>() }
+
+            Scaffold {padding ->
+                Column(modifier = Modifier.padding(16.dp)) {
+                    if (currentScreen.value == "main") {
+                        Text(
+                            text = "MYMUSIC ST10487104",
+                            style = MaterialTheme.typography.titleLarge
+                        )
         }
